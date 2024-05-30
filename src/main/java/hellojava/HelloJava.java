ef083749cf2b92dd61b4bb0506104e8499a153e1
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-05-30 16:50:23 -0400

    added a comment on the HelloJava.java

--- src/main/java/hellojava/HelloJava.java
+++ src/main/java/hellojava/HelloJava.java
@@ -2,6 +2,7 @@ package hellojava;
 
 public class HelloJava {
     public static void main (String[] args) {
+       // print "Hello, Java" to the terminal
         System.out.println("Hello Java");
     }
 }

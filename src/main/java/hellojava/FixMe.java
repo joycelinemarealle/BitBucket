fab4bea75d1f7086d79285dc4ff6186e36d3ad02
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-05-30 16:57:26 -0400

    added a FixMe file

---
+++ src/main/java/hellojava/FixMe.java
@@ -0,0 +1,9 @@
+package hellojava;
+
+public class FixMe {
+    public static void main(String[] args) {
+        System.out.println("Line one");
+        System.out.println("Line two");
+        System.out.println("Line three");
+    }
+}

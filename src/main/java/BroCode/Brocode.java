fa77fcf3c3c003eb911ba63bb402c11c01191a7a
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-06-03 17:18:41 -0400

    Added loops code

--- src/main/java/BroCode/Brocode.java
+++ src/main/java/BroCode/Brocode.java
@@ -1,19 +1,36 @@
 package BroCode;
+import java.util.Scanner;
+public class Brocode {
+    public static void main(String[] args) {
 
-public class Brocode {public static void main(String[] args){
-    String x = "water";
-    String y = "Kool- Aid";
-    String temp; //temp to store all x in temp
-    temp = x;
-    x = y; //now x can be filed with y since x is empty
-    System.out.println("x:" +x);
-    System.out.println("y:" +y);
+        String x = "water";
+        String y = "Kool- Aid";
+        String temp; //temp to store all x in temp
+        temp = x;
+        x = y; //now x can be filed with y since x is empty
+        System.out.println("x:" + x);
+        System.out.println("y:" + y);
 
-    System.out.println("\nUser input");
-    //import java.util.Scanner;
-    //Scanner scanner = new Scanner(System.in);
-   // System.out.println("What is your name?");
-   // String name = scanner.nextLine();
-   // System.out.println("Hello" +name);
-}
+        System.out.println("\nUser input");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+        //testing prime number
+        System.out.println("\n Testing a prime");
+        boolean isPrime = true;
+        for ( int i = 2; i < 100; i++){
+            for (int j = 2; j < i; j ++){
+                if (i % j == 0 ) {
+                    isPrime = false;
+                }
+            if (isPrime){
+                System.out.println(i + "Is prime");
+            }
+
+            }
+        }
+
+    }
 }
---
+++ src/main/java/hellojava/Loops.java
@@ -0,0 +1,40 @@
+package hellojava;
+
+public class Loops {
+    public static void main (String[] args) {
+        //question 1
+        int i = 1;
+        int count = 0;
+        while (i < 21) {
+            count = count + i;
+            i++;
+        }
+        System.out.println("the sum between 1 and 20 is " + count);
+
+        // question 2
+        System.out.println("\n Start of question 2");
+        for (int v = 1; v < 101; v++) { //can't initialize outsize since need the v value formula???
+            int sum = 0;
+            int formula = (v * (v + 1)) / 2;
+
+            //Calculate the sum manually
+            for (int k=1; k <= v; k++) {
+                sum = sum + k;
+
+            }//compare results
+            if (sum != formula) {
+                System.out.println("Error: Formula does not match");
+                System.out.println("The sum is : " +sum);
+                System.out.println ("The formula value is : " + formula);
+            }
+            else {
+                System.out.println("The sum: " + sum + " is equal to formula: " + formula);
+
+            }
+
+            }
+        }
+    }
+
+
+

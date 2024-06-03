df7a58fa3641891ef99e9aa922b3cfd6b328700b
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-06-03 14:57:38 -0400

    Added If Extension exercise

--- src/main/java/hellojava/Shop.java
+++ src/main/java/hellojava/Shop.java
@@ -1,19 +1,38 @@
 package hellojava;
-
 public class Shop {
-    record Sale (String item, float price, int quantity) {
+    record Sale(String item, float price, int quantity) {
+        public Sale { // conditions under record
+            if (quantity <= 0) {
+                System.out.println("Error. Quantity must be greater than 0");
+            }
+            else if (quantity > 10) {
+                price = price * 0.80f;//apply 20% discount so price now 90%
+                System.out.print("10% discount applied");
+            }
+            else if (quantity > 5) {
+                price *= 0.90f;//apply 10% discount so price now 80%
+                System.out.print("10% discount applied");
+
+            }
+        }
     }
     public static void main(String[] args) {
-        Sale sale1 = new Sale ( " flour ", 3.25f, 10); // always create  a new
-        Sale sale2 = new Sale ( " juice ", 5.6f ,3 );
+        Sale sale1 = new Sale(" flour ", 3.25f, 15); // always create  a new
+        Sale sale2 = new Sale(" juice ", 5.6f, 3);
+
+        System.out.println(" ");
+        //print sale1 and sale2
         System.out.println(sale1);
         System.out.println(sale2);
+
+        //Calculate costs
+        System.out.println();
         float cost1 = (sale1.price * sale1.quantity);
         float cost2 = (sale2.price * sale1.quantity);
         float totalCost = cost1 + cost2; //definite type of value
+
+        //Print results
         System.out.println("Item " + sale1.item + " Total cost : $ " + cost1);
-        System.out.println( "Item " +sale2.item + " Total cost : $ " + cost2);
-
-
+        System.out.println("Item " + sale2.item + " Total cost : $ " + cost2);
     }
 }

d0b3a972764d284e41399dff30001645091c2bc7
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-06-04 11:09:50 -0400

    For Exercises Agechecker added code on catching non digit apart from exit

--- src/main/java/hellojava/AgeChecker.java
+++ src/main/java/hellojava/AgeChecker.java
@@ -6,6 +6,7 @@ public class AgeChecker {// do not need to say AgeChecker.Java since know class 
     public static void main(String [] args) { // space before {
         Scanner scanner = new Scanner(System.in);
             String userInput =  ""; //define variable received fromuser
+             boolean digitValue = true; //meaning digit value inserted. The non digit accepted is only exit anything else error
             while (!userInput.equals("exit")) {
                 System.out.println("What is your age?");
                 //read as string
@@ -14,17 +15,31 @@ public class AgeChecker {// do not need to say AgeChecker.Java since know class 
                 if (userInput.equalsIgnoreCase("exit")) {   //ignore it EXIt type exIT all version capture
                     break; //end of exit typed
                 }
-                else{
-                int age = Integer.parseInt(userInput); //changing userinput to int
-                int X = 18 - age; //can initialize up to save memory because this will use more memory. after age is defined
+                //check if input from user has digit
+                //if not means a a non digit other than exit
+                //regex to search for digits in the value user types in
+
+                // assume a digit and test for applicable conditions
+                //define ditiValue before for readibility if (userInput.matches("//d+) still works but break it down
+                digitValue = userInput.matches("\\d+");
+                if (digitValue){
+                    int age = Integer.parseInt(userInput); //changing userinput to int
+                    int X = 18 - age; //can initialize up to save memory because this will use more memory. after age is defined
 
                     if (age > 18) {
-                        System.out.println("You are old enough");
+                                System.out.println("You are old enough");
                     }
                     else { //cant define int in the conditions eg else if(int x) wrong
-                    System.out.println("You must be " + X + " years older to enter"); //tab it
+                                System.out.println("You must be " + X + " years older to enter"); //tab it
                     }
+
                 }
+                // not a digit typed in so prompt user to insert a number of ext
+                //digitValue false
+                else {
+                    System.out.println("Insert a number for age or type 'exit");
+                }
+
             }
     }
 

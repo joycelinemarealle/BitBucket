eb6c650fabad900ea11961523069a6e956b8cee3
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-06-05 14:20:10 -0400

    added brocode

--- src/main/java/BroCode/Brocode.java
+++ src/main/java/BroCode/Brocode.java
@@ -13,24 +13,6 @@ public class Brocode {
 
         System.out.println("\nUser input");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name? ");
-        String name = scanner.nextLine();
-
-        //testing prime number
-        System.out.println("\n Testing a prime");
-        boolean isPrime = true;
-        for ( int i = 2; i < 100; i++){
-            for (int j = 2; j < i; j ++){
-                if (i % j == 0 ) {
-                    isPrime = false;
-                }
-            if (isPrime){
-                System.out.println(i + "Is prime");
-            }
-
-            }
-        }
 
     }
 }

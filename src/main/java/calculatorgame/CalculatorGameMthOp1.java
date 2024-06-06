abe81a136e5b4350f5040deabf643f13b6285b60
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-06-06 17:11:15 -0400

    everything mth op need to update 1 and 2

--- src/main/java/calculatorgame/CalculatorGameMthOp1.java
+++ src/main/java/calculatorgame/CalculatorGameMthOp1.java
@@ -1,17 +1,14 @@
 package calculatorgame;
+
 import java.util.Scanner;
 
-public class CalculatorGameStaticMethod {
+public class CalculatorGameMthOp1 {
     //main method referring to name and age provided in static method
     //
     public static void main (String[] args){
-        //do not need to define input inside createPlayer() not passing anything
-        // no need to define variable likes a and b because no calculatio to be done a+b
-        //initialize variable so add Payer_a since this captures String and int asking user for their age and name
-        // the three names in main player 1, player2 player3 are not connected to variables in static method p
 
         //Call the record player
-        Player_a player1 = createPlayer(); // Player_a player2 = createPlayer();Player_a player3 = createPlayer();
+        Player_b player1 = createPlayer();
         System.out.println("/n");
         System.out.println("Start of Question 3 addQuestionStatic method");
 
@@ -21,13 +18,13 @@ public class CalculatorGameStaticMethod {
 
         //Looping to ask user question length amount of time and returns score
         int resultOfQuiz= addQuiz(5);
-        HighScore_a score1 = new HighScore_a (player1, resultOfQuiz);
+        HighScore_b score1 = new HighScore_b (player1, resultOfQuiz);
         System.out.println("The highscore is" + resultOfQuiz);
 
     }
     //create a static method that calls for name and age
     // have defined a variable as Player_a which includes string and int, // then call function, // since defined it will return values
-    public static Player_a createPlayer() {
+    public static Player_b createPlayer() {
         Scanner scanner = new Scanner(System.in);
 
         // Ask user for name
@@ -40,12 +37,8 @@ public class CalculatorGameStaticMethod {
 
         //Return a player record with the information user added
         //write only once no need to create new objects each time. //The three names in main player 1, player2 player3 are not connected to variables in static method p
-        Player_a p = new Player_a(name, age);
+        Player_b p = new Player_b(name, age);
         return p;
-       // System.out.println(player1);
-       //return can only return one value
-        //????only prints one player1 so how about the rest??
-        //to return three players will need to print out three time
 
     }
 
@@ -54,20 +47,20 @@ public class CalculatorGameStaticMethod {
     public static boolean addQuestion(int a, int b){
         //initialize the two numbers user will see and be asked to add
         // no need to define a and b again already defined type variable
-        int sum = a+b;
-                // initialize the isCorrect as true. Need to define type
+        int sum = a % b;
+        // initialize the isCorrect as true. Need to define type
         //boolean isCorrect = true;
 
         //ask user to add a  and b
         Scanner scanner = new Scanner (System.in);
-        System.out.println(" Add " + a + " , " + b + " numbers together");
+        System.out.println(" Calculate remainder of dividing " + a + " , " + b);
 
         //capture useranswer
         int userAnswer = scanner.nextInt();
 
         //check to see userAnswer is correct or not
         if (userAnswer == sum){
-           return true;
+            return true;
             //return true if answer is correct
         }
         else  {
@@ -100,12 +93,28 @@ public class CalculatorGameStaticMethod {
              return the score
          */
     }
+    public static int moduloQuiz(int length){
+        //loop to call the addQuestion method length times
+        //initial count of true //yes I can call a static method within the static method. so use the function in menu that calls static method I want
+        int score = 0;
+        for(int n = 0; n < length; n++){
+
+            //calling main method addQuestion(2,2) that calls static function
+            boolean resultofuser = addQuestion(2,2); //need to pass through values
+
+            //to count amount of trues
+            if (resultofuser) { //check if true and add score. Userinput == true
+                score += 1;
+            }
+        }
+        return score;//return counts of true
 
 }
 
 //record is a classes so put outside class,es outside
-record Player_a (String name, int age){
+record Player_b (String name, int age){
 } // need {} when creating record
 
-record HighScore_a (Player_a player, int score){
+record HighScore_b (Player_b player, int score){
 } // need { when creating record*/
+

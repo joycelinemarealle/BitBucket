ee5008ebee180f9e1f2eb09c4c6429d3d9576fd2
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-06-11 12:31:26 -0400

    Project update

--- src/main/java/ProgrammingFundamentalsProject/LotterySystemProject.java
+++ src/main/java/ProgrammingFundamentalsProject/LotterySystemProject.java
@@ -11,26 +11,54 @@ public class LotterySystemProject {
     static int[] playerRandomArray = new int[6];//create new array of type int
     static int[] computerRandomArray = new int[6];
     static int[] lotteryNumbersArray = new int[6];
+    static boolean playGame ;
 
     public static void main(String[] args) {
 
-        // FR1 Over 18s only
-        System.out.println("\nStart of FR1 Over 18s only!");
+       do { //keep on playing game until user says no
+           // create new object
+           Scanner scanner = new Scanner(System.in);
 
-        // create new object
-        Scanner scanner = new Scanner(System.in);
+           // Ask user for name
+           System.out.println("Enter your name");
+           String name = scanner.nextLine();
 
-        // Ask user for name
-        System.out.println("Enter your name");
-        String name = scanner.nextLine();
+           //Validate age
+           dateValidation();
+           //call static method for playerTicket number after player  choice 1 / their own or 2/computer random number generator
+           int [] resultsOfPlayerTicketArray = playerTicketArray(); //save the returned array
+           System.out.println("\nYour Ticket Number is " + Arrays.toString(resultsOfPlayerTicketArray));
 
+           //Call static method for Lottery Numbers
+           capturedLotteryNumbers();
+           System.out.println("\nLottery Number is " + Arrays.toString(lotteryNumbersArray));
+
+           //count to see if user ticket matches that of Lottery
+           int numberOfMatches = numberOfMatchesOfLottery (lotteryNumbersArray,resultsOfPlayerTicketArray); //??? 3)issues with which parameters to pass through. want to compare lottery ticket and user ticket
+           System.out.println("\nThe number of your matches is " + numberOfMatches);
+
+           //Call static method that assigns prizes
+           String resultsOfPrizes = getPrize(lotteryNumbersArray, resultsOfPlayerTicketArray);
+           System.out.println("\n");
+           System.out.println(resultsOfPrizes);
+
+           //Ask user if want to play game again
+           System.out.println("\nWould you like to play the Lottery again? Answer Yes or No");
+           String playGameAgainChoice = scanner.nextLine();
+           if (playGameAgainChoice.equalsIgnoreCase ("No")) { //define as String which is reference so use equals a.equals('b')
+               break; //break from game
+           }
+       } while (playGame); //always run the game until user says no - they do not want play again
+
+    }
+
+    public static void dateValidation(){
         //Loop until a valid date is added and user > age
-
         LocalDate birthDate = null; //initialize birthdate as empty. put outside loop so accessible when calculating age
         boolean dateIsValid = false; //assume false until proven otherwise when a valid output is added
-
+        Scanner scanner = new Scanner(System.in);
         // Ask user for date in format Month-day-year format
-        while (!dateIsValid) { //always run when datewhile dateIsValid not true
+        while (!dateIsValid) { //start as dateIsValid is false when date is not valid
             System.out.println("\nEnter your date of birth in MONTH-DAY-YEAR format. For example 12-13-1980");
             String birthDateStr = scanner.nextLine(); // initialze name variable + get string date from user
 
@@ -38,7 +66,8 @@ public class LotterySystemProject {
             try {
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy"); //object for date formatter
                 birthDate = LocalDate.parse(birthDateStr, formatter); //converts user string to date format, if converted then date valid
-                dateIsValid = true; //date format is valid
+
+                dateIsValid = true; //date format is valid so now can calculate age
 
                 //If date valid now Calculate age
                 LocalDate today = LocalDate.now(); //?? how to check date is June 10th 2024
@@ -46,14 +75,18 @@ public class LotterySystemProject {
                 int years = age.getYears();
 
                 //Test to see if 18 years and above
-                if (years >= 18) {
+                if (years >= 18 && years < 120) { //range of one alive
                     System.out.println("\nYour age is " + years + " years");
                     break; //exit loop if age > 18
-                } else {
+
+                } else if (years > 120 ){
+                    System.out.println("\nYour age is " + years + " years. Invalid Age");
+                    dateIsValid = false; //set dateIsValid to false to ask again birthday
+                }
+                else {
                     System.out.println("\nYou need to be 18 years old to participate in the Lottery System. ");
                     birthDateStr = scanner.nextLine(); // Ask again user to their date of birth
                 }
-
                 //if date not valid then print error and prompt user to enter valid date
             } catch (DateTimeParseException e) {
                 System.out.println("Invalid Date. Enter a valid date");
@@ -61,56 +94,30 @@ public class LotterySystemProject {
             }
         }
 
-        //FR2 Pick Numbers
-        System.out.println("\nFR2Start of Pick numbers");
-
-        //Call static method that captures users in array. No need parameters since not returning anything
-        computerArray();
-        System.out.println("\n Computer generated your ticket. Your Ticket Number is " + Arrays.toString(computerRandomArray)); //print out the array with 6 random numbers
     }
 
-    //Call static method for Lottery Numbers
-    capturedLotteryNumbers();
-        System.out.println("Lottery numbers"+Arrays.toString(lotteryNumbersArray));
+    public static int[] playerTicketArray() { //no  void since returning array
+        //ask user if want to choose random or let computer generate
+            System.out.println("Select 1 if want to generate your own 6 random numbers");
+            System.out.println("Select 2 if want computer to generate your 6 random numbers");
+            Scanner scanner = new Scanner(System.in);
+            int playerchoice = scanner.nextInt();
 
-    //count to see if user ticket matches that of Lottery
-    int numberOfMatches = numberOfMatchesOfLottery();
-        System.out.println(numberOfMatches);
+            //Depending on number selected, pick appropriate random generator option
+            if (playerchoice == 1) {  //Ask user to choose random number 6 times
+                System.out.println("\nChoose 6 random numbers between 1- 49");
+                capturedNumbersArray(); //Call static method that captures users in array. No need parameters since not returning anything
+                System.out.println("\nYou added your own random numbers. Your Ticket Number is " + Arrays.toString(playerRandomArray));
+                return playerRandomArray; // need to return player generated number array need to use it in the comparison of lottery matches
 
-    public static int[] ticketArray() {
-        //ask user if want to choose random or let computer generate?
-        // Random Generator Menu options
-        System.out.println("Select 1 if want to generate your own 6 random numbers");
-        System.out.println("Select 2 if want computer to generate your 6 random numbers");
-        Scanner scanner = new Scanner(System.in);
-        int playerchoice = scanner.nextInt();
+            } else { //FR3 Lucky dip
+                System.out.println("\nStart of Lucky Dip");
+                computerArray();  //Call static method that captures users in array. No need parameters since not returning anything
+                System.out.println("\n Computer generated your ticket. Your Ticket Number is " + Arrays.toString(computerRandomArray)); //print out the array
+                return computerRandomArray; //return computer generated number array since need to use it in the comparison of lottery matches
+            }// use if or else since need to always return something. This avoids situation where user type other number than 1 or 2
 
-        //Depending on number selected, pick appropriate random generator
-        if (playerchoice == 1) {
-            //Ask user to choose random number 6 times
-            System.out.println("\nChoose 6 random numbers between 1- 49");
-
-            //Call static method that captures users in array. No need parameters since not returning anything
-            capturedNumbersArray();
-            System.out.println("\nYou added your own random numbers. Your Ticket Number is " + Arrays.toString(playerRandomArray));
-        } else if (playerchoice == 2) {
-            //FR3 Lucky dip
-            System.out.println("\nStart of Lucky Dip");
-
-            //Call static method that captures users in array. No need parameters since not returning anything
-            computerArray();
-            System.out.println("\n Computer generated your ticket. Your Ticket Number is " + Arrays.toString(computerRandomArray)); //print out the array with 6 random numbers
-        }
-        //FR3 Lucky dip
-        System.out.
-
-                println("\nStart of Lucky Dip");
-
-//Call static method that captures users in array. No need parameters since not returning anything
-        computerArray();
-        System.out.
-
-                println("\n Computer generated your ticket. Your Ticket Number is " + Arrays.toString(computerRandomArray)); //print out the array with 6 random numbers
+         //1)??? how do I return an array that captures either option 1 player inserts own numbers or 2. players ask computer to generate. I need it to compare with Lottery ticket
     }
 
     public static void capturedNumbersArray() {   //add void since not returning anything //no parameters added since no calculations done
@@ -134,7 +141,6 @@ public class LotterySystemProject {
             } while (!newNumber); // ?? While not newNumber ( not true) means when duplicate number entered do ..
 
             playerRandomArray[i] = numberChosenByPlayer; // if we've got here then the user has entered a number not in the array //insert number into array input = to what players adds
-
 
         }
     }
@@ -167,7 +173,6 @@ public class LotterySystemProject {
 
     //Static Random number for Lottery System
     public static void capturedLotteryNumbers() { //add void since not returning anything
-        System.out.println("Lottery number:");
         int lotteryRandomNumber = 0;
         boolean lotteryNumberIsNew;
 
@@ -192,18 +197,66 @@ public class LotterySystemProject {
     }
 
     //Static method to do number of counts
-    public static numberOfMatchesOfLottery() {
-        int count = 0;
+    public static int numberOfMatchesOfLottery(int[] lotteryNumbersArray, int[] resultsOfPlayerTicketArray) { //return array so not void
+        int count = 0; //use nested to test matches regardless of their positions
         for (int a = 0; a < lotteryNumbersArray.length; a++) {
-            if ((lotteryNumbersArray[a] == computerRandomArray[a]) || (lotteryNumbersArray[a] == playerRandomArray[a])) {
-                count += 1;
+            for ( int b = 0 ; b< resultsOfPlayerTicketArray.length;  b++){
+                if (lotteryNumbersArray[a] == resultsOfPlayerTicketArray[a]) {
+                    count += 1;
+                    break; //stop inner loop if match found
+                }
+
             }
+
         }
         return count;
     }
+
+    //Static method to assign prize of number of counts to applicable prices
+    //do use previous method if count > 1,23,??
+
+     public static String getPrize(int[] lotteryNumbersArray, int[] resultsOfPlayerTicketArray){ //pass the parameters
+
+         int numberOfMatches = numberOfMatchesOfLottery(lotteryNumbersArray, resultsOfPlayerTicketArray); //pass paramters
+        String prize = " ";
+          if (numberOfMatches == 2) {
+              prize = " You have 2 Matches. Your Prize is $1";
+              return prize;
+        }
+         else if (numberOfMatches == 3 ) {
+              prize = " You have 3 Matches. Your Prize is $2";
+             return prize;
+        }
+         else if ( numberOfMatches ==4) {
+             prize = " You have 4 Matches. Your Prize is $3";
+            return prize;
+        }
+         else if (numberOfMatches ==5 ) {
+             prize = " You have 5 Matches. Your Prize is $8";
+              return prize;
+
+        }
+         else if (numberOfMatches == 6) {
+             prize =" You have 6 Matches. Your Prize Jackpot";
+             return prize;
+
+        }
+         else {
+             prize = "Sorry, You won no prize. Please try again";
+             return  prize;
+
+         }
+     }
 }
 
+/*//Call static method that captures player's random numbers in array. No need parameters since not returning anything
+        //capturedNumbersArray();
+        //System.out.println("You entered your own 6 random numbers. Your Ticket Number is " + Arrays.toString(playerRandomArray));
 
+        //Call static method computer generated numbers in array. No need parameters since not returning anything
+        //computerArray();
+        //System.out.println("\n Computer generated your ticket. Your Ticket Number is " + Arrays.toString(computerRandomArray)); //print out the array with 6 random numbers
+*/
 //FR3 Lucky dip Static Method
 
 /*Capture user age

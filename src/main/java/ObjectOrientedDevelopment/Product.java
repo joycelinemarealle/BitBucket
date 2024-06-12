b98174e8ea4e9e120c9fc4bb37a1b16cec6170f0
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-06-12 17:31:27 -0400

    OOP Record and class project

--- src/main/java/ObjectOrientedDevelopment/Product.java
+++ src/main/java/ObjectOrientedDevelopment/Product.java
@@ -1,7 +1,35 @@
-//package ObjectOrientedDevelopment;
+package ObjectOrientedDevelopment;
 
-//public class Product {
- //   public static void main (String[]{
+public class Product { //defined as private. These are data field
+    private String name; //need to be private
+    private double price;
+    private int quantity;
+
+    //define constructor that takes in data field. This is a method that holds data field and can call it
+    public Product(String name, double price, int  quantity) {
+        this.name = name; //this refers to data field above
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    //define getter that return the data field
+    public String getName() {
+        return this.name;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public int getQuantity(){
+        return this.quantity;
+    }
+    @Override
+    public String toString(){
+        return " [name " + name + "price=" + price + "quantity=" + quantity +"]";
+    }
+    void print() {
+        System.out.println(name + " " + price + " " + quantity);
+    }
+}
 
 
 

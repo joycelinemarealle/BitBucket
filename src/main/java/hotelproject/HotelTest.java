50037db273065317f222ca341c6fb3b30dd6c2c5
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-06-21 17:12:24 -0400

    testing

--- src/main/java/hotelproject/HotelTest.java
+++ src/main/java/hotelproject/HotelTest.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class HotelTest {
+public class HotelTest {  //testing comment
     public static void main(String[] args) {
 
     //booking Menu

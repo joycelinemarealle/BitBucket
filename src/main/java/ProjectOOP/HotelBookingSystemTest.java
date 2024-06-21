e6cac656afca79d7c63453f8ff878e8ad2cf8757
joycelinemarealle <joyceline.marealle@peopleshores.com>
2024-06-21 08:09:57 -0400

    updated to oop hotel booking project

--- src/main/java/ProjectOOP/HotelBookingSystemTest.java
+++ src/main/java/ProjectOOP/HotelBookingSystemTest.java
@@ -6,14 +6,26 @@ import java.util.List;
 public class HotelBookingSystemTest {
     public static void main(String[] args) {
 
-        //make empty array that carries elements of type room
 
-       List<Room> rooms = new ArrayList<>();
-       rooms.add(new Room("Double Standard", 150));
+        Hotel hotel = new Hotel ("Luxury Hotel"); //define objects to use methods in hotel class
 
-        Room room1 = new Room("Double Standard ", 150);
+        //Hotel rooms
+       Room room1 = new Room ( 1, "Double Standard", 150);
+       Room room2 = new Room ( 2, "Double Standard", 150);
+       Room room3 = new Room ( 3, "Deluxe Double", 200);
+       Room room4 = new Room ( 4, "Deluxe Double", 200);
+       Room room5 = new Room ( 5, "Junior Suite", 300);
+       Room room6 = new Room ( 6, "Grand Suite", 400);
 
+       //adding the six rooms in
+        hotel.addRoom(room1);
+        hotel.addRoom(room2);
+        hotel.addRoom(room3);
+        hotel.addRoom(room4);
+        hotel.addRoom(room5);
+        hotel.addRoom(room6);
 
+/*while true ask Main menu secl 1 book room 2.see confirmation 3.eexist, 4 enter choice*/
 
 
     }
--- src/main/java/ProjectOOP/Room.java
+++ src/main/java/ProjectOOP/Room.java
@@ -1,18 +1,29 @@
 package ProjectOOP;
 
 public class Room {
-    //attribute
+    //attributes of room
+    private int roomId;
     private String roomType;
     private double price;
+    private boolean isBooked;
 
-    //constructor
+//constructor
 
-    public Room(String roomType, double price) {
+    public Room(int roomId, String roomType, double price) {
+        this.roomId = roomId;
         this.roomType = roomType;
+        this.isBooked = false; //set up default so do not need to pass it as parameter
         this.price = price;
     }
 
-    //getters and setters
+    //Getters and setters
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
 
     public String getRoomType() {
         return roomType;
@@ -30,13 +41,24 @@ public class Room {
         this.price = price;
     }
 
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(boolean booked) {
+        isBooked = booked;
+    }
+
     //Override to String
 
     @Override
     public String toString() {
         return "Room{" +
-                "roomType='" + roomType + '\'' +
+                "roomId=" + roomId +
+                ", roomType='" + roomType + '\'' +
                 ", price=" + price +
+                ", isBooked=" + isBooked +
                 '}';
     }
 }
+

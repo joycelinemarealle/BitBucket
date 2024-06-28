852326a2e7376f96d8200e5cd5b90cfaf55ada41
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-06-28 12:23:42 -0400

    JM-9 IO handling qn 2

--- src/main/java/JavaIOFileHandling/InputOutput.java
+++ src/main/java/JavaIOFileHandling/InputOutput.java
@@ -5,44 +5,41 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-
 
 public class InputOutput {
     public static void main(String[] args) {
-    // C:\\Users\\JoycelineMarealle\\files\\Alice.txt
 
-//        Path path = Paths.get("C:\\Users\\JoycelineMarealle\\files\\Alice.txt");
-//        Path path2 = Path.of("/Users", "JoycelineMarealle", "files", "Alice.txt");
+        String path = "C:\\files\\Alice.txt";
+        System.out.println("The total word count is " + wordCount(path));
+    }
 
-        try (BufferedReader br = Files.newBufferedReader(Path.of("C:\\files\\Alice.txt"))){
+    //Static method for wordCount
+    public static int wordCount(String path) {
+        int wordTotal;
+        try (BufferedReader br = Files.newBufferedReader(Path.of(path))) {
+            String line;
+            wordTotal = 0;
 
-        //try (BufferedReader br = Files.newBufferedReader (path)){
-           String line;
-           int wordCount = 0;
-           int lineCount = 0;
+            //read every line and then store in string line
+            while ((line = br.readLine()) != null) {
 
-
-           //read every line and then store in string line
-           while ((line = br.readLine()) != null){
-
-               /*words in a word array
-               * get length of word array in each line and add */
-               String[] words = line.split(" ");
-               wordCount = wordCount + words.length;
+                /*words in a word array get length of word array in each line and add */
+                String[] words = line.split(" ");
+                wordTotal = wordTotal + words.length;
 
             }
-            System.out.println("The total word count is " + wordCount);
-       } catch ( FileNotFoundException e){
-           e.printStackTrace();
 
-       } catch (IOException e){
-           e.printStackTrace();
-       }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return wordTotal;
     }
 }
 /*
 * Store txt in folder in This PC\Local disk\Users\JoycelineMarealle\files
 * split lines
 * count words in each line
-* add the words to get total*/
+* add the words to get total*
+//        Path path = Paths.get("C:\\Users\\JoycelineMarealle\\files\\Alice.txt");
+//        Path path2 = Path.of("/Users", "JoycelineMarealle", "files", "Alice.txt"); */

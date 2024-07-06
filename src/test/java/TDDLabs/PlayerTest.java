c681384f078c873cc4f0112a38a5b57627f265ec
joy <joyceline.marealle@peopleshores.com>
2024-07-06 13:24:24 -0400

    JM-12 testing

--- src/test/java/TDDLabs/PlayerTest.java
+++ src/test/java/TDDLabs/PlayerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PlayerTest {
+public class PlayerTest { //testing
 
     @Test
     public void getName() {

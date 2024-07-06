84eb2c94ac5b8403917024edb47762889d5bd7ff
joycelinemarealle <joyceline.marealle@peopleshores.com>
2024-07-06 09:50:45 -0400

    JM-12 TDD labs Player test files

--- src/test/java/TDDLabs/PlayerTest.java
+++ src/test/java/TDDLabs/PlayerTest.java
@@ -9,7 +9,21 @@ public class PlayerTest {
 
     @Test
     public void getName() {
-        Player player1 = new Player(" Joy ");
-        assertEquals("Joy", player1.getName());
+        Player player = new Player(" Joy ");
+        assertEquals("Joy", player.getName());
     }
+
+    @Test
+    public void getPhoneNumber() {
+        Player player = new Player("Joy", 123456789);
+        assertEquals(123456789, player.getPhoneNumber());
+
+    }
+
+    @Test
+    public void isFreeAgent() {
+        Player player = new Player("Joy");
+      assertEquals( true,  player.isFreeAgent());
+    }
+
 }

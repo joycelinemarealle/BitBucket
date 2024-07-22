0d462416ab3e2ba87de2124837358cdc9ca36bc3
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-07-22 16:04:30 -0400

    JM-17 Part 2 Gilt Portoflio Class Implementation

--- giltsportfolio/src/main/java/gilts/GiltPortfolio.java
+++ giltsportfolio/src/main/java/gilts/GiltPortfolio.java
@@ -30,16 +30,43 @@ public class GiltPortfolio {
     }
 
 
-//methods
-    public void buyGilt(Gilt g) {
+//Buy Gilt method
+    public void buyGilt(Gilt g)   {
+        //get price
+        double price = pricingEngine.getPrice(g);
 
+        //if price >= than balance then add gilt in portfolio
+        if ( this.balance >= price ){
+            portfolio.add(g);
+            balance -= price;
+        } else {
+            throw new CantAffordGiltException();
+        }
     }
 
+    //Sell Gilt Method
     public void sellGilt(Gilt g) {
-
+        double price = pricingEngine.getPrice(g);
+if ( portfolio.remove(g)){
+    balance += price;
+}
     }
 
+    //Tick method
     public void tick() {
+        //collect interest, check if gilt expired, remove from portfolio
+        List <Gilt > expiredGilts = new ArrayList<>();
+        //Loop through each gilt in portfolio and add interest to balance
+        for (Gilt gilt : portfolio){
+            this.balance += gilt.tick();
+            //check if gilt expired and if year add to list for expired gilts
+if (gilt.expired()){
+    expiredGilts.add(gilt);
+}
+        }
+
+        //Remove all expired Gilts from portfolio array list
+        portfolio.removeAll(expiredGilts);
 
     }
 }

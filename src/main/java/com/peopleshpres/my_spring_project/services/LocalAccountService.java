1400c09ae6904caf691a68a7159e7d8cefa4a77f
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-08-14 15:58:19 -0400

    JM-21 logger

--- src/main/java/com/peopleshpres/my_spring_project/services/LocalAccountService.java
+++ src/main/java/com/peopleshpres/my_spring_project/services/LocalAccountService.java
@@ -2,30 +2,32 @@ package com.peopleshpres.my_spring_project.services;
 
 import com.peopleshpres.my_spring_project.entities.Account;
 import com.peopleshpres.my_spring_project.repos.AccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.logging.Logger;
+
 
 @Service
-public class LocalAccountService implements AccountService{
+public class LocalAccountService implements AccountService {
     private final AccountRepository accountRepository;
+    private final Logger logger = LoggerFactory.getLogger(LocalAccountService.class);
 
 
     //spring boost know that we are using AccountRepository  for injection
     //injection in constructor recommended
 
-   // @Autowired
-   public LocalAccountService(AccountRepository accountRepository){
-       this.accountRepository = accountRepository;
-   }
+    // @Autowired
+    public LocalAccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 
     @Override
     public List<Account> retrieveAllAccounts() {
-        Logger logger ;
         logger.info("Getting accounts from Account Repository");
-        System.out.println("Getting accounts from Account Repository");
+        //System.out.println("Getting accounts from Account Repository");
         return this.accountRepository.getAllAccounts();
     }
 }

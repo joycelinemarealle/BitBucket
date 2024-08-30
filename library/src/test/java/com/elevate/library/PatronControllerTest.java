72b4e8a360597d4a6f25363be72fc16c49b0db3b
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-08-30 17:18:04 -0400

    JM-23 tests

--- library/src/test/java/com/elevate/library/PatronControllerTest.java
+++ library/src/test/java/com/elevate/library/PatronControllerTest.java
@@ -1,4 +1,47 @@
 package com.elevate.library;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class PatronControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    PatronDAO patrons;
+
+    private Patron patron;
+
+    @BeforeEach
+    void setUp(){
+        patron = new Patron (1,"Jojo");
+
+    }
+    @Test
+    void getPatronById() {
+        when(patrons.getById(1)).thenReturn(patron);
+
+        RequestBuilder request = MockMvcRequestBuilders.get("/patrons/10").accept(MediaType.APPLICATION_JSON);
+
+        try {
+            mockMvc.perform(request)
+                    .andExpect(jsonPath("/patrons/10").value("Jojo"))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
---
+++ library/src/test/java/com/elevate/library/PatrondEndToEndTest.java
@@ -0,0 +1,7 @@
+package com.elevate.library;
+
+public class PatrondEndToEndTest {
+
+    //@Autowire PatronDAo patrond
+    // real object testing not a mock bean
+}
